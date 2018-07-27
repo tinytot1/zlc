309467bc5401e03e05a185a89ef1ffe4c5839f4e
张怀 <904169395@qq.com>
2018-07-27 18:20:59 +0800

    静态服务器

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,15 +3,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: "development", // "production" | "development" | "none"
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        a: './server.js'
     },
     devtool: 'inline-source-map',
+    devServer: {
+        hot: true
+    },
+
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
+            title: 'Output Management',
+            filename: "test.html"
+            // template: 'index.html'
         })
     ],
     output: {
