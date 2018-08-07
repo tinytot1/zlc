9f5c1b1fbcf067a0c41a6700fb87fcf958adaf22
张怀 <904169395@qq.com>
2018-08-07 10:51:59 +0800

    version

--- dist/common/common.js
+++
@@ -1,1 +0,0 @@
-console.log("a.js"),console.log("a.js"),console.log("a.js"),console.log("a.js"),console.log("b.js"),console.log("b.js"),console.log("b.js"),console.log("b.js"),console.log("b.js"),console.log("main.js");
